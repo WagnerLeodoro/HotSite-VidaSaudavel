d121b2274c4be9003553da3972f8014f032199a9
WagnerLeodoro <wagner.augustoleodoro@gmail.com>
2022-04-30 23:14:59 -0300

    Validação assíncrona do formulário com JQuery e Tratamento de erros

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,27 @@
-﻿
+﻿$(document).ready(function () {
+    $("#frmCadastro").submit(function (e) {
+        e.preventDefault();
+        Cadastrar();
+    });
+});
+
+function Cadastrar() {
+    let params = {
+        Nome: $("#nome").val(),
+        Email: $("#email").val(),
+        Mensagem: $("#mensagem").val(),
+    };
+    $.post("/Home/Cadastrar", params)
+        .done(function (data) {
+            if (data.status == "SUCCESS") {
+                $("#frmCadastro").after("<h3>Agradecemos o seu contato!</h3>");
+                $("#frmCadastro").hide();
+            }
+            else {
+                alert(data.mensagem);
+            }
+        })
+        .fail(function () {
+            alert("Erro desconhecido, tente mais tarde!");
+        })
+};
